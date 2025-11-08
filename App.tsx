f48ef9f5fd7e73f7849c44b806acda14967fcb13
Fadi Subair <fadisubair929@gmail.com>
2025-11-08 19:26:48 +0530

    fix: sidebar scroll issue fixed

--- App.tsx
+++ App.tsx
@@ -55,7 +55,7 @@ const App: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col md:flex-row min-h-screen text-black dark:text-white font-sans antialiased bg-white dark:bg-black">
+    <div className="flex flex-col md:flex-row h-screen text-black dark:text-white font-sans antialiased bg-white dark:bg-black">
       <Sidebar 
         activeMode={viewMode} 
         onModeChange={handleViewChange} 
@@ -63,8 +63,12 @@ const App: React.FC = () => {
         onThemeChange={handleThemeChange}
         isComposerEnabled={!!analysisResponse}
       />
-      <div className="flex-1 overflow-x-hidden">
-        <div className="min-h-screen dark:bg-grid-white/[0.05] bg-grid-black/[0.05] relative">
+
+      {/* This is now your scrolling content area */}
+      <div className="flex-1 overflow-x-hidden overflow-y-auto">
+        
+        {/* We removed min-h-screen from this div */}
+        <div className="dark:bg-grid-white/[0.05] bg-grid-black/[0.05] relative">
           <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
           <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-4 sm:py-6 lg:py-8 relative z-10">
             <main>
